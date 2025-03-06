382b1eb4a9af69ac1234e9f025055d18b5ac54e1
Mangonando <juan.f.ramirezv@gmail.com>
2025-03-06 23:07:13 +0100

    Start weather app project

---
+++ TogetherWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TogetherWeather
+//
+//  Created by Juan Ramirez on 06.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TogetherWeather/TogetherWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogetherWeatherApp.swift
+//  TogetherWeather
+//
+//  Created by Juan Ramirez on 06.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TogetherWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
